e49a66a09dcce7f44e6106abab638d4f98e309f0
NguyenDuc1003 <20223502@eaut.edu.vn>
2025-12-17 13:31:39 +0700

    upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        float rate = 23000; // Tỷ giá USD sang VND
+        Console.WriteLine("Chuyển đổi tiền tệ");
+        Console.WriteLine("Nhập vào số tiền cần chuyển đổi");
+        double amount = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Số tiền đã nhập: " + amount);
+        Console.WriteLine("Tỷ giá chuyển đổi: " + rate);
+        Console.WriteLine("Tiền sau khi chuyển đổi: " + amount * rate);
+
+    }
+}
